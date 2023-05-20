30ec3c5b97e2278ca754312e5f6513fa2420b88b
Santiago Di Deo <101676324+SantiagoDiDeo@users.noreply.github.com>
2023-05-20 20:36:36 -0300

    creacion de server

---
+++ src/server.js
@@ -0,0 +1,49 @@
+import http from 'http';
+import cluster from 'cluster';
+import logger from './logger/logger.js';
+import {PORT} from './environments/environments.js';
+import app from './config/apConfig.js';
+import { initializeWebsockets } from '../config/socketConfig.js';
+
+const httpServer = http.createServer(app);
+initializeWebsockets(httpServer);
+
+httpServer.on('error', (error) => {
+    logger.error('ocurrio un error: ', error);
+});
+
+/* cluster | server on */
+
+let mode = 'FORK';
+
+if (process.argv.length > 2) {
+
+    const procArgv = process.argv[2].toUpperCase();
+    console.log(procArgv)
+    if (procArgv === 'CLUSTER') {
+        mode = 'CLUSTER';
+    };
+};
+
+if (mode === 'CLUSTER') {
+        if (cluster.isPrimary) {
+            const numCPUs = require('os').cpus().length;
+                
+            for (let i = 0; i < numCPUs; i++) {
+                cluster.fork();
+            }
+            cluster.on('exit', (worker) => {
+                logger.info(`Process ${worker.process.pid} died`);
+                cluster.fork();
+            });
+            httpServer.listen(PORT, () => {
+                logger.info(`Servidor en modo cluster corriendo en el proceso ${process.pid}`);
+            });
+        } else {
+            httpServer.listen(PORT, () => {
+            const horaActual = new Date().toLocaleTimeString();
+            console.log(` (${horaActual}) Servidor en modo fork corriendo en el proceso ${process.pid} en puerto ${PORT}`)
+            logger.info(`Servidor en modo fork corriendo en el proceso ${process.pid} en puerto ${PORT}`);
+        });
+    };
+};
