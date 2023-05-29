2c0c5f279216f65a1072351d73562389bf8babe8
Santiago Di Deo <101676324+SantiagoDiDeo@users.noreply.github.com>
2023-05-29 18:20:50 -0300

    arreglo errores (con errores) y agregado de chats y cart

---
+++ routes/chatRouter.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import passport from '../DB/config/auth.js';
+
+import logger from '../logger/logger.js';
+import { getAllChatsByUserController } from '../controller/chats.js';
+import { addMessageController } from '../controller/chats.js';
+
+
+const {Router} = express;
+const chatRouter = Router();
+
+chatRouter.use(passport.initialize());
+chatRouter.use(passport.session());
+
+chatRouter.get('/', async (req, res) => {
+    try {
+        res.render('chat.handlebars');
+    } catch (error) {
+        logger.error(`${error} --Error al traer chats`);
+    }
+});
+
+chatRouter.get('/:username', async (req, res) => { //falta passport.authenticate
+    try {
+        const {username} = req.params;
+        const chats = await getAllChatsByUserController(username);
+        res.render('chat', {chats});
+    } catch (error) {
+        logger.error(`${error} --Error al buscar chat por params.username`);
+    }
+});
+
+chatRouter.post('/:username/:msg', async (req, res) => { //falta passport.authenticate
+        try {
+            const username = req.params.username
+            const messageToAdd = await addMessageController(username, req.params.msg)
+            res.status(200).send(messageToAdd);
+        } catch (error) {
+            logger.error(`${error} --Error al intentar agregar chats a db route`);
+        }
+});
+
+export default chatRouter;
