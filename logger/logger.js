d118ccb5a83efbe348c40afd17dd0b3f99a44903
Santiago Di Deo <101676324+SantiagoDiDeo@users.noreply.github.com>
2023-05-29 18:19:49 -0300

    arreglo error en logs

--- logger/logger.js
+++ logger/logger.js
@@ -5,6 +5,7 @@ import log4js from 'log4js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const logDir = path.join(__dirname, 'logs');
+console.log("🚀 ~ file: logger.js:8 ~ logDir:", logDir)
 
 log4js.configure({
     appenders: {
@@ -12,17 +13,30 @@ log4js.configure({
         infoFile: {type: 'file', filename: path.join(logDir, 'info.log')},
         warnFile: { type: 'file', filename: path.join(logDir, 'warn.log') },
         errorFile: { type: 'file' , filename: path.join(logDir, 'error.log') },
-        consoleLogger: { type: 'logLevelFilter', appender: 'infoFile', level: 'info' },
+        infoFileLogger: { type: 'logLevelFilter', appender: 'infoFile', level: 'info' },
         warnFileLogger: { type: 'logLevelFilter', appender: 'warnFile', level: 'warn' },
         errorFileLogger: { type: 'logLevelFilter', appender: 'errorFile', level: 'error' }
     }, 
     categories: {
         default: {
-        appenders: ['consoleLogger', 'warnFileLogger', 'errorFileLogger'],
-        level: 'all'
+            appenders: ['console', 'infoFileLogger', 'errorFileLogger', 'warnFileLogger'],
+            level: 'all'
+            },
+            info: {
+                appenders: ['infoFileLogger', 'console'],
+                level: 'info'
+            },
+            warn: {
+                appenders: ['warnFileLogger', 'console'],
+                level: 'warn'
+            },
+            error: {
+                appenders: ['errorFileLogger', 'console'],
+                level: 'error'
+            }
         }
     }
-});
+);
 
 const logger = log4js.getLogger();
 
