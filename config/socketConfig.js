5f55bab64b92d6639720495b5622f48499077771
Santiago Di Deo <101676324+SantiagoDiDeo@users.noreply.github.com>
2023-05-29 18:22:41 -0300

    arreglos en configuraciones

--- config/socketConfig.js
+++ config/socketConfig.js
@@ -9,10 +9,53 @@ export const initializeWebsockets = (httpServer) => {
     io.on('connection', async (socket) => {
         logger.info(`New connection id: ${socket.id}`);
 
-        socket.on('newChat', async (msg) => {
-            await getAllChatsByUserController();
-            const chatToAdd = await addMessageController( msg.username, 'user', msg.body);
+        socket.on('online', async (username) => {
+            const allChats = await getAllChatsByUserController(username);
+            socket.emit('chat', allChats);
+        });
+
+        socket.emit('chat', async (msg) => {
+            allChats.push({
+                username: username,
+                body: msg.body,
+            })
+            socket.emit('chat', allChats);
+        })
+
+        socket.on('newChat', async (username) => {
+            const allChats = await getAllChatsByUserController(username);
+            const chatToAdd = await addMessageController( username, 'user', msg.body);
             io.sockets.emit('newChat', chatToAdd);
         });
+
+        socket.on('connect', () => {
+            console.log('socket connected');
+        });
+        
+        socket.on('chat', async (data) => {
+            
+            htmlToRender = '';
+            await data.forEach((element) => {
+        
+                htmlToRender = htmlToRender + `
+                <tr>
+                    <th><h1 class='user'>${element.username}</h1></th>
+                    <th><h1 class='mensaje'>${element.message}</h1></th>
+                </tr>
+                `
+            });
+        
+            document.getElementById('message').innerHTML = htmlToRender;
+            });
+        const addMessage = async (messageToAdd) => {
+        
+            messageToAdd =  {
+                username: userEmail.value,
+                body: userMensaje.value
+                }
+            if(messageToAdd.username) {
+                await socket.emit('newMessage', messageToAdd);
+            };
+        };
     });
 };
